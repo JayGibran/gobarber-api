ad73af3f79e1cacaa6bbc195ebd616b52c790d7b
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-06-30 16:17:44 +0100

    adding gobarber api environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'fc39bd9322cde0',
-    pass: '522af3c0187769',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
