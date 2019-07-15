7e20995c4b535fa27502bd45534d9810cadbc789
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-07-15 22:26:45 +0100

    doing smalls refactor to integrate with frontend

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -50,7 +51,9 @@ class UserController {
     }
 
     const { email, oldPassword } = req.body;
+
     const user = await User.findByPk(req.userId);
+
     if (email !== user.email) {
       const userExists = await User.findOne({
         where: { email: req.body.email },
@@ -59,12 +62,20 @@ class UserController {
         return res.status(400).json({ error: 'User already exists' });
       }
     }
+
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
-    const { id, name, provider } = await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        { mode: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+      ],
+    });
+
+    return res.json({ id, name, email, avatar });
   }
 }
 
